ca0a9461a1c9fc0e0f21d1dae24694f7bc1fbe0a
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-03 16:22:13 -0500

    Add show action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,4 +15,9 @@ class SongsController < ApplicationController
     )
     render :show
   end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+    render :show
+  end
 end
--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -15,4 +15,12 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
       assert_response 200
     end
   end
+
+  test "show" do
+    get "/songs/#{Song.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "artist", "album", "year", "created_at", "updated_at"], data.keys
+  end
 end
