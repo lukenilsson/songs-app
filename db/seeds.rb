88dc7b133a0aee627bbadc930560deef9a52ae8a
“lukenilsson” <luke.nilsson@gmail.com>
2023-03-03 15:15:01 -0500

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Song.create(
+  title: "Penny Lane",
+  artist: "The Beatles",
+  album: "Penny Lane / Strawberry Fields Forever",
+  year: 1967,
+)
+
+Song.create(
+  title: "Beacon",
+  artist: "Matt Duncan",
+  album: "Beacon",
+  year: 2010,
+)
+
+Song.create(
+  title: "The Joke",
+  artist: "Brandi Carlile",
+  album: "By The Way, I Forgive You",
+  year: 2018,
+)
